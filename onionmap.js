d3ffc85ee6a93dd8f6b0ac770fb7748a708bf439
opi <opi@no-log.org>
2015-09-16 22:30:58 +0200

    Initial commit.

---
+++ onionmap.js
@@ -0,0 +1,96 @@
+// Create map
+var map = L.map('map').setView([27, 18], 2);
+
+// Add background map layer
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by<a href="http://cartodb.com/attributions#basemaps">CartoDB</a>, under <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>. Data by <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, under ODbL.',
+    minZoom: 1,
+    maxZoom: 10
+}).addTo(map);
+
+// Control scale
+L.control.scale({
+    position: 'bottomleft',
+    metric: true,
+    imperial: false,
+    updateWhenIdle: true
+}).addTo(map);
+
+// Set map on loading state
+console.log(map.getContainer().classList);
+map.getContainer().classList.add('loading');
+
+// Get relay list from onionoo
+// Real url is https://onionoo.torproject.org/details?fields=nickname,fingerprint,latitude,longitude,consensus_weight_fraction,guard_probability,middle_probability,exit_probability,dir_address,country,country_name,as_number,as_name
+var jsonPath = "https://onionoo.torproject.org/details?fields=nickname,fingerprint,latitude,longitude,consensus_weight_fraction,guard_probability,middle_probability,exit_probability,dir_address,country,country_name,as_number,as_name";
+// var jsonPath = 'onionoo.json';
+fetchJSONFile(jsonPath, function(data) {
+
+    // Remove loading state
+   map.getContainer().classList.remove('loading');
+
+    // Update information panel
+    document.getElementById('bridge-count').innerHTML = data.bridges.length;
+    document.getElementById('relay-count').innerHTML = data.relays.length;
+
+    // New feature group
+    relaysMarkers = L.featureGroup();
+
+    // Loop through relays
+    data.relays.forEach(function(relay, idx){
+        // // debug
+        // if (idx < 5) console.log(relay);
+
+        if (relay.latitude && relay.longitude) {
+            var marker = L.marker([relay.latitude, relay.longitude])
+                                        .bindPopup(
+                                            "<h3>"+relay.nickname+"</h3><br />"+
+                                            ""+relay.fingerprint+"<br />"+
+                                            "country: "+relay.country_name+" ("+relay.country+")<br />"+
+                                            "as_number: "+relay.as_number+"<br />"+
+                                            "as_name: "+relay.as_name+"<br />"+
+                                            "consensus_weight_fraction: "+relay.consensus_weight_fraction+"<br />"+
+                                            "guard_probability: "+relay.guard_probability+"<br />"+
+                                            "middle_probability: "+relay.middle_probability+"<br />"+
+                                            "exit_probability: "+relay.exit_probability+"<br />"
+                                        );
+            // Add relay to feature group
+            relaysMarkers.addLayer(marker);
+        }
+    });
+
+
+    // Marker cluster group
+    var relaysCluster = new L.MarkerClusterGroup({
+        showCoverageOnHover: true,
+        polygonOptions: {
+            stroke: false,
+            opacity: 0.2,
+            fillColor: '#885CA4',
+            fillOpacity: 0.1
+        },
+        maxClusterRadius: 60,
+    });
+
+    // Add relay markers to cluster
+    relaysCluster.addLayer(relaysMarkers);
+
+    // Add cluster to map
+    map.addLayer(relaysCluster);
+});
+
+
+/** Utility **/
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+}
